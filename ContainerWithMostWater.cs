a7b10c61f6c1e3fd18b165fe53b65f80d0e45f29
mahmoud gamal <107665999+mahmoudgamal007@users.noreply.github.com>
2022-11-12 09:48:57 +0200

    Add files via upload

---
+++ ContainerWithMostWater.cs
@@ -0,0 +1,43 @@
+﻿namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int head = 0;
+            int tail=height.Length - 1;
+            int maxwater = 0;
+           while(head!=tail)
+            {
+                int max = 0;
+                if (height[head]<height[tail])
+                {
+                    max=height[head]*(tail-head);
+                    head++;
+                }
+                else if(height[head]>height[tail])
+                {
+                    max=height[tail]*(tail-head);
+                    tail--;
+                }
+                else
+                {
+                    max=height[head]*(tail-head);
+                    tail--;
+                }
+                if (max>maxwater)
+                    maxwater=max;
+            }
+            return maxwater;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 5,1,1,1 };
+            Console.WriteLine(Solution.MaxArea(arr));
+            
+        }
+    }
+}
